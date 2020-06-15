4e074e83aa5ffbde92e2e05dea2aba87a39e45f7
dirtydirtydan <danielcubillos95@gmail.com>
2020-06-14 20:59:26 -0500

    Updates

---
+++ precache-manifest.6f696527e831076a69d9e3e005468600.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e34d945895c5b56ef46df62a6260c13",
+    "url": "/covid2020/index.html"
+  },
+  {
+    "revision": "e89f30c8e975b654c1ab",
+    "url": "/covid2020/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "35bb72b2a85d8333080e",
+    "url": "/covid2020/static/js/2.7e5944ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid2020/static/js/2.7e5944ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e89f30c8e975b654c1ab",
+    "url": "/covid2020/static/js/main.c8dff637.chunk.js"
+  },
+  {
+    "revision": "04b196a1f361cfd85b58",
+    "url": "/covid2020/static/js/runtime-main.9d36940d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
